6ce193eaf9ae0c79dae94a3545e4affc473fc8ec
Kcrespo15 <kevincrespo15@gmail.com>
2022-09-19 19:50:14 -0400

    updated employee seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,15 +6,15 @@ VALUES (1, "Sales"),
 (4, "Legal");
 
 -- EMPLOYEE ROLE SEEDS 
-INSERT INTO role (id, title, salary, department_id)
-VALUES (1, "Sales Lead", 150000, 1),
-(2, "Salesperson", 100000, 1),
-(3, "Lead Engineer", 200000, 2),
-(4, "Software Engineer", 150000, 2),
-(5, "Account Manager", 125000, 3),
-(6, "Accountant", 100000, 3),
-(7, "Legal Team Lead", 300000, 4),
-(8, "Lawyer", 175000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 150000, 1),
+("Salesperson", 100000, 1),
+("Lead Engineer", 200000, 2),
+("Software Engineer", 150000, 2),
+("Account Manager", 125000, 3),
+("Accountant", 100000, 3),
+("Legal Team Lead", 300000, 4),
+("Lawyer", 175000, 4);
 
 -- EMPLOYEE SEEDS 
 INSERT INTO employee (id, first_name, last_name, role_id)
