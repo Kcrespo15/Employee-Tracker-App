4d4bd197407473f72101ea7a5e0f4c7a4230dfc6
Kcrespo15 <kevincrespo15@gmail.com>
2022-09-20 20:20:23 -0400

    intial prompts and switch cases

--- sever.js
+++ sever.js
@@ -1,6 +1,6 @@
 // INSTALL DEPENDENCIES
-const inquirer = require("inquirer")
-const mysql = require("mysql")
+const inquirer = require("inquirer");
+const mysql = require("mysql");
 const cTable = require('console.table');
 
 // CREATING CONNECTION TO SERVER
@@ -19,3 +19,76 @@ const connection = mysql.createConnection({
     init();
 });
 
+// INITIAL PROMPTS & SWITCH CASE
+function init() {
+    inquirer.prompt([
+    {
+    type: "list",
+    message: "What would you like to do?",
+    name: "choice",
+    choices: [
+              "View All Employees?", 
+              "View All Employee's By Roles?",
+              "View all Emplyees By Deparments", 
+              "Update Employee",
+              "Add Employee?",
+              "Add Role?",
+              "Add Department?"
+            ]
+    }
+
+    ]).then(function(val) {
+        switch (val.choice) {
+            case "View All Employees":
+              viewAllEmployees();
+            break;
+    
+          case "View All Employee's By Roles":
+              viewAllRoles();
+            break;
+
+          case "View all Emplyees By Deparments":
+              viewAllDepartments();
+            break;
+          
+          case "Add Employee":
+                addEmployee();
+              break;
+
+          case "Update Employee":
+                updateEmployee();
+              break;
+      
+            case "Add Role":
+                addRole();
+              break;
+      
+            case "Add Department":
+                addDepartment();
+              break;
+    
+            }
+    })
+}
+
+// View All Employees
+function viewAllEmployees() {
+    connection.query("SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;", 
+    function(err, res) {
+      if (err) throw err
+      console.table(res)
+      init()
+  })
+}
+
+// View All Roles
+function viewAllRoles() {
+    connection.query("SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;", 
+    function(err, res) {
+    if (err) throw err
+    console.table(res)
+    init()
+    })
+  }
+
+//  View All Employees By Departments
